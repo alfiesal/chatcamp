1f865f124b2131d8c60d7126feff9f8659670ca7
Pawel Brzoski <pawel.brzoski@profitroom.com>
2023-03-11 22:40:03 +0100

    list improve

--- public/subscribe.php
+++ public/subscribe.php
@@ -2,7 +2,7 @@
 
 $email = $_POST['email'];
 
-file_put_contents('list.txt', $email."\n", FILE_APPEND);
+file_put_contents('../list.txt', $email."\n", FILE_APPEND);
 
 header('Location: thanks.php');
 die();
