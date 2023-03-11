73cf52dfdc6f4266964131a567bbbb082107a43b
Pawel Brzoski <pawel.brzoski@profitroom.com>
2023-03-11 22:53:46 +0100

    list improve

--- public/subscribe.php
+++ public/subscribe.php
@@ -1,8 +1,31 @@
 <?php
 
+include '../dupa.php';
+
 $email = $_POST['email'];
 
-file_put_contents('../list.txt', $email."\n", FILE_APPEND);
+
+$servername = "localhost";
+$username = "root";
+$password = PASSEK;
+$dbname = "camp";
+
+// tworzenie połączenia z bazą danych
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+// ustawianie trybu raportowania błędów na wyjątki
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// przygotowanie zapytania SQL z parametrami powiązanymi
+$stmt = $conn->prepare("INSERT INTO clients (email) VALUES (:email)");
+
+// ustawienie wartości parametrów za pomocą metody bindParam
+$stmt->bindParam(':email', $email);
+
+
+// wykonanie zapytania SQL
+$stmt->execute();
+
 
 header('Location: thanks.php');
 die();
